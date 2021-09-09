caf0003ab94b3a1a53703f99ae9e4a2894fcd783
Stan <guseletovS@gmail.com>
2021-09-09 17:41:35 +0100

    finally works!!!!!!!!!

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
     // try... catch documentation:
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
     try {
-      const res = await fetch(`https://tranquil-tundra-77719.herokuapp.com/${endpoint}`);
+      const res = await fetch(`https://tranquil-tundra-77719.herokuapp.com${endpoint}`);
       const body = await res.json();
       setMessage(body.message);
       setDigipetStats(body.digipet);
